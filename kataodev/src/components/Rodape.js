ea2fdf98039744d2919addf4f91ce6925c47eb0f
Felipe Catão do Nascimento <cataofelipe@gmail.com>
2020-12-18 21:29:05 -0300

    update footer

---
+++ kataodev/src/components/Rodape.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../CSS/components.css'
+
+function Rodape(){
+    return(
+        <footer>Hello</footer>
+    )
+}
+
+export default Rodape
--- kataodev/src/pages/Mainpage.js
+++ kataodev/src/pages/Mainpage.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import Banner from '../components/Banner.js'
+import Rodape from '../components/Rodape.js'
 
 function MainPage(){
     return(
         <>
         <Banner></Banner>
         <div className="gridColuns">
-            <div className="articlesCreated"></div>
+            <div className="articlesCreated">
+            </div>
             <div className="articlesCreated"></div>
         </div>
+        <Rodape></Rodape>
         </>
     )
 }
 
 export default MainPage
+
+
