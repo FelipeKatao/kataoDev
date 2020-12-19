9ad7f209f285299caddd6faf501a0e7e10656181
Felipe Catão do Nascimento <cataofelipe@gmail.com>
2020-12-18 21:37:21 -0300

    update test with for

---
+++ kataodev/src/components/Articlesmini.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ArticlesMini(){
+    return(
+        <>
+        <div>Hello world</div>
+        <br></br>
+        </>
+    )
+}
+
+export default ArticlesMini
--- kataodev/src/pages/Mainpage.js
+++ kataodev/src/pages/Mainpage.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import Banner from '../components/Banner.js'
 import Rodape from '../components/Rodape.js'
+import ArticlesMini from '../components/Articlesmini.js'
 
 function MainPage(){
+    var elements = []
+    for (var i=0;i<5;i++){
+        elements.push(<ArticlesMini></ArticlesMini>)
+    }
     return(
         <>
         <Banner></Banner>
         <div className="gridColuns">
             <div className="articlesCreated">
+                {elements}
             </div>
             <div className="articlesCreated"></div>
         </div>
