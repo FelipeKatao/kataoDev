a5e1111487615f1301e770757afe113069bd049c
Felipe Catão do Nascimento <cataofelipe@gmail.com>
2020-12-28 18:56:02 -0300

    create virtual agent

--- kataodev/src/App.js
+++ kataodev/src/App.js
@@ -3,11 +3,13 @@ import MenuFixed from './components/MenuFixed.js'
 import { Switch, Route} from 'react-router-dom';
 import  About from './pages/About.js'
 import MainPage from './pages/Mainpage.js'
+import Robot from './components/Robot.js'
 
 function App() {
   return (
     <>
     <MenuFixed></MenuFixed>
+    <Robot></Robot>
     <Switch>
       <Route path='/' component={MainPage}></Route>
       <Route path='/about' component={About}></Route>
---
+++ kataodev/src/components/Robot.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Robot(){
+    return(
+        <div id="robot">
+            <img id="robotImg" alt="Ai inteligencia" src="https://img1.gratispng.com/20180529/gsx/kisspng-robot-youtube-kissoro-tribal-game-cute-robot-5b0d1b75e57a24.57913442152758565394.jpg" height="100vh" width="100vh"></img>
+        </div>
+    )
+}
+
+export default Robot
